eadb7659a607229259423621e637022ae182da8c
MohamedHamouch <med10hamouch@gmail.com>
2025-03-25 11:30:16 +0000

    create tournament and game migrations

--- database/migrations/2025_03_25_112307_create_tournaments_table.php
+++ database/migrations/2025_03_25_112307_create_tournaments_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tournaments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
     }
