418b17dff749b135d88eafbda9fc6f97e0633d46
MohamedHamouch <med10hamouch@gmail.com>
2025-03-27 14:25:36 +0000

    protect routes using JST auth middleware

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\TournamentController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\GameController;
@@ -9,11 +9,15 @@ use App\Http\Controllers\GameController;
 //auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
-Route::get('/user', [AuthController::class, 'user'])->middleware('auth:api');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('jwt.auth');
+Route::get('/user', [AuthController::class, 'user'])->middleware('jwt.auth');
+
+// Route::get('/test', function () {
+//     return response()->json(['Stauts' => 'You are allowed']);
+// });
 
 //tournament
-Route::middleware('auth:api')->group(function () {
+Route::middleware('jwt.auth')->group(function () {
     Route::post('/tournaments', [TournamentController::class, 'store']);
     Route::get('/tournaments', [TournamentController::class, 'index']);
     Route::get('/tournaments/{id}', [TournamentController::class, 'show']);
